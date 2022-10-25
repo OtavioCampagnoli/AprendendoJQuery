7b708f6a984b54ac6c454a58ae7ebcb0acd85293
Otávio Henrique Moreira Campagnoli <otavio.Campagnoli@rift.local>
2022-10-25 16:01:01 -0300

    Introdução: Sintaxa básica.

---
+++ 001-Introducao/PrimeiroExemplo/SintaxaBasica.js
@@ -0,0 +1,29 @@
+/*
+A jQuery é uma biblioteca JavaScript com o objetivo de simplificar a escrita de scripts client-side, facilitando a manipulação de elementos presentes em páginas HTML - seja para adicionar efeitos, comportamentos ou animações que aprimoram a experiência do usuário.
+
+Para importar a biblioteca jQuery para um projeto basta utilizarmos a tag script apontando para o arquivo da biblioteca, da seguinte forma:
+*/
+
+// 1- Importar diretamente do repositório do jQuery:
+<script src = "https://code.jquery.com/jquery-3.1.1.min.js"></script>
+
+//2- Importar com arquivo local:
+
+//<script src = "jquery-3.1.1.min.js"></script>
+
+/*
+Para iniciar a implementação com jQuery, é necessário definir o escopo global por meio do código apresentado a seguir e que deve ficar no final da página:
+
+<script>
+
+$(document).ready(function(){
+//implementação em jQuery
+});
+
+</script>
+*/  
+
+$(document).ready(function(){
+    //implementação em jQuery
+    });
+
