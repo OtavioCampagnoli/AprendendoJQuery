9281c57f530477559d184f251e375f86e456eb94
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2022-11-04 12:57:14 -0300

    Projeto landing page parte 1.

---
+++ 011-ProjetoLandingPage/js/script.js
@@ -0,0 +1,12 @@
+$(document).scroll(function ()
+{
+    var topWindow = $(window).scrollTop();
+    if(topWindow > 60)
+    {
+        $("header").addClass('header_fixed');
+    }
+    else
+    {
+        $("header").removeClass('header_fixed');
+    }
+});
